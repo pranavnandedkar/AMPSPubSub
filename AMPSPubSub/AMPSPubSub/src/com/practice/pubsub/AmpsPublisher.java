ca612e73b6e02da4a3d58a23deb8a43aea40aaeb
pranav <pranav@pranav-PC>
2016-05-22 21:12:44 -0400

    second push with amps.xml

--- AMPSPubSub/AMPSPubSub/src/com/practice/pubsub/AmpsPublisher.java
+++ AMPSPubSub/AMPSPubSub/src/com/practice/pubsub/AmpsPublisher.java
@@ -5,13 +5,16 @@ import com.crankuptheamps.client.exception.AMPSException;
 
 public class AmpsPublisher
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("-----");
 		Client client = new Client("TestPublisher-Client");
 		try {
 			client.connect("tcp://192.168.40.135:9007/amps/json");
 			client.logon();
-			client.publish("messages", "{ \"message\" : \"Hello, world!\" }");
+			for(int i=0;i<999999999;i++){
+				Thread.sleep(1000);
+				client.publish("messages", "{ \"message\" : \"Hello, world!\" }");
+			}
 		}
 		catch (AMPSException aex) {
 			System.err.println("TestListener caught exception."+aex);
@@ -19,4 +22,4 @@ public class AmpsPublisher
 			client.close();
 		}
 	}
-}
+} 
